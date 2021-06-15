66add93d72b05df20aa78899a0bc69d1f2b71cfe
unknown <galeno@dcc.ufrj.br>
2021-06-15 00:09:59 -0300

    changing video and updating widgets

--- youtube/src/components/Video.js
+++ youtube/src/components/Video.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './Video.css';
 
 
@@ -6,13 +6,19 @@ import './Video.css';
 const Video = () => {
     // estado para dizer se o video acabou
     const [notFinished, setFineshed] = useState(true)
+    const [start, setStart] = useState(10)
+    const [end, setEnd] = useState(start+10)
 
-    const start =  Math.floor(Math.random() * 105);
-    const end = start + 10;
-    const videoID = 'ujf3iJoWgrM?'
-    const urlYT = 'https://www.youtube.com/embed/'
+    useEffect(() => { 
+        setStart(Math.floor(Math.random() * 105))
+        setEnd(start+10)
+    }, [start, end])
+    
+    //const start =  Math.floor(Math.random() * 105);
+    //const end = start + 10;
+    const urlYT = 'https://www.youtube.com/embed/oVNjkJmpslI?'
     const options = 'controls=0&rel=0&autoplay=1&mute=1&autohide=0&showinfo=0'
-    const url = urlYT + videoID + options + '&start=' + start.toString() + '&end=' + end.toString()
+    const url = urlYT + options + '&start=' + start.toString() + '&end=' + end.toString()
     
     setTimeout(() => {
         //alert("chamei timeout")
@@ -35,7 +41,7 @@ const Video = () => {
                             <iframe 
                                 width="560" 
                                 height="315" 
-                                src={urlYT + videoID + 'controls=0&rel=0&autoplay=1&mute=0&autohide=0&showinfo=0' + '&start=' + start.toString() + '&end=' + end.toString()} title="YouTube video player" frameborder="0" 
+                                src={urlYT + 'controls=0&rel=0&autoplay=1&mute=0&autohide=0&showinfo=0' + '&start=' + start.toString() + '&end=' + end.toString()} title="YouTube video player" frameborder="0" 
                                 allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"  
                             />
                         }       
